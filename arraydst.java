204659d01d4fa805b9d78f71dc78918879351811
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-14 11:08:53 +0530

    insert feature

--- arraydst.java
+++ arraydst.java
@@ -1,21 +1,48 @@
 import java.util.Arrays;
 
 public class arraydst {
-    public static int insert(int arr [] , int index , int value){
-        if(arr.length == index){
+
+    public static int insert(int arr[], int index, int value) {
+
+        if (arr.length == index) {
             System.out.println("Array is Full");
             return index;
         }
-        
-        
-    } 
 
-    public static void main (String[] args) {
-        int arr [] = new int [10] ;
+        arr[index] = value;
+
+        for (int i = 1; i <= index; i++) {
+
+            int curr = arr[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+
+            arr[prev + 1] = curr;
+        }
+
+        return ++index;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[10];
+
         int index = 0;
-        index = insert(arr,index,10);
-        index = insert(arr, index, 5);
+
+        index = insert(arr, index, 10);
+        index = insert(arr,index,50);
+        index = insert(arr,index,20);
+        index = insert(arr,index,30);
+        index = insert(arr,index,40);
+        index = insert(arr,index,100);
+        index = insert(arr,index,90);
+        index = insert(arr,index,80);
+        index = insert(arr,index,70);
+        index = insert(arr, index, 60);
         System.out.println(Arrays.toString(arr));
     }
 }
-
--- insertion_sort.java
+++ insertion_sort.java
@@ -5,17 +5,16 @@ public class insertion_sort {
 
         int[] arr = {5, 3, 4, 1};
 
-        for (int i = 1; i < arr.length; i++) {
-
-            int key = arr[i];
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+        for(int i = 1 ; i < arr.length ; i++){
+            int curr = arr[i] ;
+            int prev = i - 1 ;
+            // finding out correct position to insert
+            while(prev >= 0  && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--; 
             }
-
-            arr[j + 1] = key;
+            // insertion
+            arr[prev+1] = curr;
         }
 
         System.out.println(Arrays.toString(arr));
